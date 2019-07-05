0808e07e89db441e028b705672593b1323e97551
Zainab Amir <zainabamir16@outlook.com>
2019-07-05 16:18:18 +0500

    Remove overrides for __str__ and __repr__

--- openedx/core/djangoapps/system_wide_roles/models.py
+++ openedx/core/djangoapps/system_wide_roles/models.py
@@ -12,19 +12,6 @@ class SystemWideRole(UserRole):  # pylint: disable=model-missing-unicode
      .. no_pii:
     """
 
-    def __str__(self):
-        """
-        Return human-readable string representation.
-        """
-        str_representation = "<SystemWideRole {role}>"
-        return str_representation.format(role=self.name)
-
-    def __repr__(self):
-        """
-        Return uniquely identifying string representation.
-        """
-        return self.__str__()
-
 
 class SystemWideRoleAssignment(UserRoleAssignment):  # pylint: disable=model-missing-unicode
     """
@@ -33,19 +20,3 @@ class SystemWideRoleAssignment(UserRoleAssignment):  # pylint: disable=model-mis
     """
 
     role_class = SystemWideRole
-
-    def __str__(self):
-        """
-        Return human-readable string representation.
-        """
-        str_representation = "<SystemWideRoleAssignment for User {user} assigned to role {role}>"
-        return str_representation.format(
-            user=self.user.id,
-            role=self.role.name
-        )
-
-    def __repr__(self):
-        """
-        Return uniquely identifying string representation.
-        """
-        return self.__str__()
